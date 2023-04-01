75cdec96225ebb19ab3c1f8469ba238256d774c6
Aurore Philip <aurore.philip@intersec.com>
2023-04-01 14:34:58 +0200

    Add Bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import type { AppProps } from 'next/app'
 import { getLoggedUserId } from '../utils/getLoggedUserId'
+import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
+import { useEffect } from 'react'
 
 // Default way to get a logged user
 export const loggedUserId = getLoggedUserId()
+console.log(loggedUserId)
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    }, []);
   return <Component {...pageProps} />
 }
 
